579141c53627c584174c9c9c9c6e6b01938769f5
erthang <endrewrthang@gmail.com>
2019-10-11 13:07:08 -0300

    E07 adicionado

--- E07/E07.py
+++ E07/E07.py
@@ -9,10 +9,8 @@ with open("numeros.txt") as numfile:
     coordenadas.extend(map(criar_coordenada, numfile.readlines()))
 
 matriz = np.zeros((15, 16), np.float64)
-print(matriz)
 for i in range(15):
     for j in range(15):
-
         matriz[i][j] = coordenadas[i].x ** j
     matriz[i][15] = coordenadas[i].y
 
@@ -20,10 +18,17 @@ for i in range(15):
 for i in range(15):
     for j in range(i):
             const = matriz[i][j] / matriz[j][j]
-            print(const)
             for k in range(16):
                 matriz[i][k] -= const * matriz[j][k]
 
 
-print(matriz)
+for i in range(14, -1, -1):
+    matriz[i][15] /= matriz[i][i]
+    for j in range(i):
+        matriz[j][i] *= matriz[i][15]
+        matriz[j][15] -= matriz[j][i]
+        
+for i in range(15):
+    print(f"a{i:2.0f} = {matriz[i][15]:12.8f}")
+
 np.savetxt("matriz.txt", matriz, '%15.4f')
